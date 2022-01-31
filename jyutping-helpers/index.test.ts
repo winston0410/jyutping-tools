841874e1ae42584e7f5e071612129cb71c0e740e
kghugo <hugosum.dev@protonmail.com>
2022-01-31 19:06:38 +0000

    Pass all tests

--- jyutping-helpers/index.test.ts
+++ jyutping-helpers/index.test.ts
@@ -1,7 +1,43 @@
-import { jyutpingToYale } from './index'
+import { jyutpingToYale } from "./index";
 
-describe('test', () => {
-  it('should work', () => {
-    expect(1).toBe(1);
+describe("oe in Jyutping", () => {
+  it("should be converted to eu in Yale", () => {
+    //  香港
+    expect(jyutpingToYale(["hoeng1gong2"])).toEqual(["heung1gong2"]);
   });
-})
+});
+
+describe("j in Jyutping", () => {
+  it("should be converted to y in Yale", () => {
+    //  人
+    expect(jyutpingToYale(["jan4"])).toEqual(["yan4"]);
+  });
+});
+
+describe("aa in Jyutping", () => {
+  it("should be converted to a in Yale", () => {
+    //  沙
+    expect(jyutpingToYale(["saa1"])).toEqual(["sa1"]);
+  });
+});
+
+describe("z in Jyutping", () => {
+  it("should be converted to j in Yale", () => {
+    //  正
+    expect(jyutpingToYale(["zeng3"])).toEqual(["jeng3"]);
+  });
+});
+
+describe("c in Jyutping", () => {
+  it("should be converted to ch in Yale", () => {
+    //  車
+    expect(jyutpingToYale(["ce1"])).toEqual(["che1"]);
+  });
+});
+
+describe("jyu in Jyutping", () => {
+  it("should be converted to ch in Yale", () => {
+    //  魚
+    expect(jyutpingToYale(["jyu4"])).toEqual(["yu4"]);
+  });
+});
--- jyutping-helpers/index.ts
+++ jyutping-helpers/index.ts
@@ -1,5 +1,49 @@
-type ConversionFn = (inputs: Array<string>) => string;
+type ConversionFn = (inputs: Array<string>) => Array<string>;
+
+const jyutpingToYaleDict = {
+  oe: "eu",
+  j: "y",
+  aa: "a",
+  z: "j",
+  c: "ch",
+  // REF https://en.wikipedia.org/wiki/Jyutping#:~:text=sik6%2F9-,Comparison%20with%20Yale%20romanisation,nasal%20stop%3A%20m%2C%20ng.
+  //  In Jyutping, if no consonant precedes the vowel yu, then the initial j is appended before the vowel. In Yale, the corresponding initial y is never appended before yu under any circumstances.
+  jy: "y",
+};
 
 export const jyutpingToYale: ConversionFn = (inputs) => {
-  return inputs[0];
+  const result = [...inputs];
+  let index = 0;
+
+  for (const word of result) {
+    let i = 0;
+    let j = 1;
+    const mutable = [...word];
+
+    while (i < mutable.length) {
+      const cur = mutable[i];
+
+      // NOTE Check for double letters replacement first, as jyu > yu conversion will be eaten up by j -> y conversion
+      const doubleSub = jyutpingToYaleDict[`${mutable[i]}${mutable[j]}`];
+      if (doubleSub) {
+        mutable[i] = doubleSub[0];
+        mutable[j] = doubleSub[1] ?? "";
+        i++;
+        j++;
+      } else {
+        // NOTE Check for single letter replacement
+        const sub = jyutpingToYaleDict[cur];
+        if (sub) {
+          mutable[i] = sub;
+        }
+      }
+
+      i++;
+      j++;
+    }
+    result[index] = mutable.join("");
+    index++;
+  }
+
+  return result;
 };
