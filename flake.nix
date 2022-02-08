cfd09c42e70b42c496a92e766ff94208820acf36
kghugo <hugosum.dev@protonmail.com>
2022-02-08 20:13:23 +0000

    Use workspace

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "preview wasm app";
+
+  inputs = { nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; }; };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShell.${system} = (({ pkgs, ... }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # For compatibility with Vercel
+            nodejs-16_x
+          ];
+          shellHook = ''
+
+          '';
+        }) { inherit pkgs; });
+    };
+}
