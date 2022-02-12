8f2b45d288d0c1347beaa4c04d3f34e3669bef4e
kghugo <hugosum.dev@protonmail.com>
2022-02-12 15:06:10 +0000

    Remove redundant option

--- microservice/src/routes/convert.rs
+++ microservice/src/routes/convert.rs
@@ -4,39 +4,25 @@ use serde::{Deserialize, Serialize};
 // use crate::services::{chars_to_jyutping, chars_to_yale};
 use crate::services::{chars_to_jyutping, chars_to_yale, CharsToJyutpingResult};
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub enum JyutpingSystem {
-    Jyutping,
-    Yale,
-}
-
 #[derive(Debug, Deserialize)]
 pub struct RequestQuery {
-    to: Option<JyutpingSystem>,
     input: String,
 }
 
 #[derive(Debug)]
 pub struct UnwrappedQuery {
-    to: JyutpingSystem,
     input: String,
-}
-
-impl From<RequestQuery> for UnwrappedQuery {
-    fn from(orig: RequestQuery) -> Self {
-        let to = orig.to.unwrap_or(JyutpingSystem::Jyutping);
-        Self {
-            to,
-            input: orig.input,
-        }
-    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ConvertResponseBody {
-    system_used: JyutpingSystem,
     results: CharsToJyutpingResult,
+}
+
+impl From<RequestQuery> for UnwrappedQuery {
+    fn from(orig: RequestQuery) -> Self {
+        Self { input: orig.input }
+    }
 }
 
 //NOTE Request body is not used for GET, only request URI will be considered
@@ -46,15 +32,9 @@ pub async fn convert_chars_to_jyutping(
 ) -> Result<HttpResponse, HttpError> {
     let unwrapped_query = UnwrappedQuery::from(query.into_inner());
 
-    let converted = match unwrapped_query.to {
-        JyutpingSystem::Jyutping => chars_to_jyutping(unwrapped_query.input.to_owned()),
-        JyutpingSystem::Yale => chars_to_yale(unwrapped_query.input.to_owned()),
-    };
+    let results = chars_to_jyutping(unwrapped_query.input.to_owned());
 
-    Ok(HttpResponse::Ok().json(ConvertResponseBody {
-        system_used: unwrapped_query.to,
-        results: converted,
-    }))
+    Ok(HttpResponse::Ok().json(ConvertResponseBody { results }))
 }
 
 //NOTE Use GET as it is the only cacheable response
