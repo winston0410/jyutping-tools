549108b0e5c2c77f7e2dd00d9b69f31037fdf045
kghugo <hugosum.dev@protonmail.com>
2022-02-17 16:48:31 +0000

    Add longest segementor

--- wordseg/src/lib.rs
+++ wordseg/src/lib.rs
@@ -1,9 +1,68 @@
-pub struct Model {}
+use std::collections::HashSet;
+use unicode_segmentation::UnicodeSegmentation;
 
-impl Model {
+pub struct Segmenter {
+    max_word_length: usize,
+    model: HashSet<String>,
+}
+
+impl Segmenter {
+    //REF https://stackoverflow.com/questions/28469667/borrowed-value-does-not-live-long-enough-when-using-the-builder-pattern
+    // Have to return orignal copy to avoid reference error
+
+    /// Set the maximum word length for segmentation. The maximum word length cannot be smaller
+    /// than 2, as it will be meaningless for segmentation
+    pub fn max_word_length(mut self, max_word_length: usize) -> Segmenter {
+        if max_word_length < 2 {
+            panic!("max_word_length must be >= 2 to be meaningful")
+        }
+        self.max_word_length = max_word_length;
+        self
+    }
+
     /// Train the model by inputting existing result
-    pub fn fit(&self, input: Vec<String>) {}
+    pub fn fit(&mut self, segmented: Vec<Vec<&str>>) {
+        for s in segmented.into_iter() {
+            for word in s.into_iter() {
+                if word.graphemes(true).count() > 1 {
+                    self.model.insert(word.to_owned());
+                }
+            }
+        }
+    }
 
     /// Insert unhanlded text for getting prediction
-    pub fn predict(&self, input: String) {}
+    pub fn predict(&self, unsegmented: String) -> Vec<String> {
+        let mut result: Vec<String> = Vec::new();
+        let unicoded = unsegmented.graphemes(true);
+        let input_length = unicoded.to_owned().count();
+        let max_word_length = std::cmp::min(self.max_word_length, input_length);
+
+        // i pointer match the beginning indice of a word
+        let mut i: usize = 0;
+        // j pointer match the end indice of a word
+        let mut j: usize = i + max_word_length;
+
+        while i < input_length {
+            let segment = unicoded.to_owned().skip(i).take(j - i).collect::<String>();
+            if self.model.contains(&segment) || j - i == 1 {
+                result.push(segment.to_owned());
+                i = j;
+                j = i + max_word_length;
+            } else {
+                j = j - 1
+            };
+        }
+
+        return result;
+    }
+}
+
+impl Default for Segmenter {
+    fn default() -> Self {
+        Segmenter {
+            max_word_length: 2,
+            model: HashSet::new(),
+        }
+    }
 }
---
+++ wordseg/tests/integration_test.rs
@@ -0,0 +1,35 @@
+use wordseg::Segmenter;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[should_panic]
+    fn should_panic_when_given_invalid_max_word_length() {
+        wordseg::Segmenter::default().max_word_length(0);
+    }
+
+    #[test]
+    fn should_predicts_english() {
+        let mut segmenter = wordseg::Segmenter::default().max_word_length(4);
+
+        segmenter.fit(vec![
+            vec!["this", "is", "a", "sentence"],
+            vec!["that", "is", "not", "a", "sentence"],
+        ]);
+
+        let result = segmenter.predict("thatisadog".to_owned());
+
+        assert_eq!(result, vec!["that", "is", "a", "d", "o", "g"]);
+    }
+
+    #[test]
+    fn should_predicts_unicode_characters() {
+        let mut segmenter = wordseg::Segmenter::default().max_word_length(4);
+
+        segmenter.fit(vec![vec!["我", "係", "香港", "人"]]);
+
+        let result = segmenter.predict("佢哋係香港人".to_owned());
+
+        assert_eq!(result, vec!["佢", "哋", "係", "香港", "人"]);
+    }
+}
