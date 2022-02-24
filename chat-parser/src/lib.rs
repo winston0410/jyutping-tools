b4aec374ffb4386128cf3ab160cf7a39c4af9047
kghugo <hugosum.dev@protonmail.com>
2022-02-24 13:27:01 +0000

    Define shape for parser

--- chat-parser/src/lib.rs
+++ chat-parser/src/lib.rs
@@ -7,7 +7,23 @@ mod predicates;
 use nom::branch::alt;
 use nom::IResult;
 
-pub struct Parser {}
+// enum ParserToken {
+// Meta { encoding: String },
+// Data (),
+// }
+
+// pub struct Parser {
+// // Should expect ParserToken::meta
+// meta: ParserToken,
+// // Should expect ParserToken::data
+// data: ParserToken,
+// }
+//
+
+pub struct Parser {
+    meta: Meta,
+    data: Vec<Token>,
+}
 
 impl Parser {
     pub fn parse(raw_input: &str) -> () {
