df60d0a497a5493b4236a78a0f41d6fa5d7a3272
kghugo <hugosum.dev@protonmail.com>
2022-02-25 09:52:29 +0000

    Update

---
+++ blog/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts" context="module">
+	import '@fontsource/open-sans';
+</script>
+
+<main>
+    <slot />
+</main>
+
+<style>
+	:global(body) {
+		font-family: 'Open Sans', sans-serif;
+	}
+</style>
