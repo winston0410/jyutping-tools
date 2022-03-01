3a74de8a93015b4ffee2696cfa6aec7011657980
kghugo <hugosum.dev@protonmail.com>
2022-03-01 22:46:07 +0000

    Fix

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,26 +1,11 @@
 <script lang="ts" context="module">
-	export async function load() {
-		return {
-			status: 302,
-			redirect: '/services/cantonese-to-jyutping'
-		};
-	}
 </script>
 
 <script lang="ts">
-	const links = [
-		{
-			name: 'Segment Cantonese',
-			href: '/services/segment-cantonese'
-		},
-		{
-			name: 'Cantonese to Jyutping',
-			href: '/services/cantonese-to-jyutping'
-		}
-	];
 </script>
 
-<!--  <h1>Jyutping Tools</h1>  -->
+<h1>Cantonese tools</h1>
+
 <!--  <p>This website contains tools to help you process Cantonese for learning purposes.</p>  -->
 <!--  <ul role="list">  -->
 <!--  {#each links as { name, href }}  -->
@@ -32,5 +17,6 @@
 <!--  {/each}  -->
 
 <!--  </ul>  -->
+
 <style lang="scss">
 </style>
