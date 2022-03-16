55eef172488a1f2fd09c2c05f1714785e8be9e93
kghugo <hugosum.dev@protonmail.com>
2022-03-16 23:06:31 +0000

    Try

--- rscantonese/data/words.hk/postprocess.ts
+++ rscantonese/data/words.hk/postprocess.ts
@@ -1,3 +1,22 @@
+// NOTE FlatAction will run this script from root
+// cwd: /home/runner/work/jyutping-tools/jyutping-tools
 const cwd = Deno.cwd();
 
-console.log(`cwd: ${cwd}`)
+console.log(`cwd: ${cwd}`);
+
+const ALL_DATA_ZIP_PATH = `${cwd}/rscantonese/data/words.hk/all.csv.gz`;
+
+const unzipProcess = Deno.run({
+  cmd: ["gunzip", ALL_DATA_ZIP_PATH],
+  stdout: "piped",
+  stderr: "piped",
+});
+
+try {
+    const output = await unzipProcess.output()
+    const error = await unzipProcess.stderrOutput()
+    console.log(`check unzip stdout ${output}`)
+    console.log(`check unzip stderr ${error}`)
+} catch (error) {
+    console.log('unexpected error: ', error)
+}
