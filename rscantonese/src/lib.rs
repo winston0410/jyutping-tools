a73d9efe8fcb884642bb1da4f2ded7c5652b1dcb
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-04-17 19:27:46 +0100

    Add tch

--- rscantonese/src/lib.rs
+++ rscantonese/src/lib.rs
@@ -1,94 +1,29 @@
-use tokenizers::decoders::bpe::BPEDecoder;
-use tokenizers::decoders::DecoderWrapper;
-use tokenizers::models::bpe::BpeTrainer;
-use tokenizers::models::bpe::BPE;
-use tokenizers::normalizers::BertNormalizer;
-use tokenizers::normalizers::NormalizerWrapper;
-use tokenizers::pre_tokenizers::bert::BertPreTokenizer;
-use tokenizers::pre_tokenizers::PreTokenizerWrapper;
-use tokenizers::processors::bert::BertProcessing;
-use tokenizers::processors::PostProcessorWrapper;
-use tokenizers::tokenizer::{AddedToken, EncodeInput, Tokenizer};
-use tokenizers::TokenizerBuilder;
 use typed_builder::TypedBuilder;
-// use std::path::PathBuf;
 pub mod utils;
 mod constant;
 
-// #[derive(TypedBuilder)]
-/// Configuration for training RsCantonese tokenizer
+#[derive(TypedBuilder)]
+/// Configuration for training RsCantonese transformer
 pub struct RsCantoneseConfig {
-    /// Path for saving the trained artifect. Default to `$TMP_DIR/rscantonese.json`
-    model_path: std::path::PathBuf,
-    // vocab_size: usize,
-    files: Vec<String>,
+
 }
 
 impl RsCantoneseConfig {
-    pub fn train(&self) -> RsCantonese {
-        let mut trainer = BpeTrainer::builder()
-            // .vocab_size(self.vocab_size)
-            .special_tokens(vec![AddedToken::from("UNK", true)])
-            .build();
 
-        let mut tokenizer_builder = TokenizerBuilder::<
-            BPE,
-            NormalizerWrapper,
-            PreTokenizerWrapper,
-            PostProcessorWrapper,
-            DecoderWrapper,
-        >::new()
-        .with_model(BPE::default()).build().unwrap();
-        // .with_normalizer(Some(NormalizerWrapper::Sequence(
-        //     tokenizers::normalizers::utils::Sequence::new(vec![NormalizerWrapper::BertNormalizer(
-        //         BertNormalizer::new(true, true, None, true),
-        //     )]),
-        // )))
-        // .with_pre_tokenizer(Some(PreTokenizerWrapper::Sequence(
-        //     tokenizers::pre_tokenizers::sequence::Sequence::new(vec![
-        //         PreTokenizerWrapper::BertPreTokenizer(BertPreTokenizer),
-        //     ]),
-        // )))
-        // .with_post_processor(Some(PostProcessorWrapper::Bert(BertProcessing::default())))
-        // .with_decoder(Some(DecoderWrapper::BPE(BPEDecoder::default())))
-        // .build()
-        // .unwrap();
-
-        tokenizer_builder
-            .train_from_files(&mut trainer, self.files.to_owned())
-            .unwrap()
-            .save(&self.model_path, false)
-            .unwrap();
-
-        let tokenizer = Tokenizer::from_file(&self.model_path).unwrap();
-
-        RsCantonese { tokenizer }
-    }
 }
 
-impl Default for RsCantoneseConfig {
-    fn default() -> Self {
-        let mut model_path = std::env::temp_dir();
-        model_path.push(constant::MODEL_PATH);
 
-        RsCantoneseConfig {
-            model_path,
-            files: Vec::new(),
-        }
-    }
-}
+// pub struct RsCantonese {
+//     tokenizer: Tokenizer,
+// }
 
-pub struct RsCantonese {
-    tokenizer: Tokenizer,
-}
-
-/// Struct that contains the tokenizer. This should be created from RsCantoneseConfig.train()
-impl RsCantonese {
-    pub fn segment<'s, T>(&self, input: T) -> Vec<String>
-    where
-        T: Into<EncodeInput<'s>>,
-    {
-        let encoding = self.tokenizer.encode(input, false).unwrap();
-        encoding.get_tokens().to_vec()
-    }
-}
+// /// Struct that contains the tokenizer. This should be created from RsCantoneseConfig.train()
+// impl RsCantonese {
+//     pub fn segment<'s, T>(&self, input: T) -> Vec<String>
+//     where
+//         T: Into<EncodeInput<'s>>,
+//     {
+//         let encoding = self.tokenizer.encode(input, false).unwrap();
+//         encoding.get_tokens().to_vec()
+//     }
+// }
--- rscantonese/tests/integration_test.rs
+++ rscantonese/tests/integration_test.rs
@@ -1,17 +1,25 @@
 #[cfg(test)]
 mod tests {
+    use rust_bert::xlnet::XLNetConfig;
+    use rust_bert::xlnet::XLNetForTokenClassification;
+    use std::path::Path;
+    use tch::{nn, Device};
+
     #[test]
     fn should_segment_correctly() {
+        // let config = XLNetConfig::default();
+
+        
         // let token = std::env::var("GITHUB_TOKEN").unwrap();
 
         // let fetcher = rscantonese::utils::GitHubFetcher::new(&token);
 
         // let assets_path = &fetcher.get("https://raw.githubusercontent.com/winston0410/nlp-data/develop/wikidump.tar.gz");
 
-        let rscantonese = rscantonese::RsCantoneseConfig::default().train();
-        let result = rscantonese.segment("我係香港人。");
+        // let rscantonese = rscantonese::RsCantoneseConfig::default().train();
+        // let result = rscantonese.segment("我係香港人。");
 
-        assert_eq!(result, ["我", "係", "香港人", "。"]);
+        // assert_eq!(result, ["我", "係", "香港人", "。"]);
 
         assert_eq!(true, false);
     }
