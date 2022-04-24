5d068c1808a5d7353e5b98bc413f290d751cc0e8
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-04-24 13:22:13 +0100

    Saving dataset

--- datasets/index.py
+++ datasets/index.py
@@ -1,15 +1,14 @@
 from datasets import load_dataset
 from operator import itemgetter
 
-mock_dataset_raw = load_dataset("text", data_files="mock_data.txt")
+dataset_raw = load_dataset("text", data_files="mock_data.txt")
 
-mock_dataset = mock_dataset_raw["train"].train_test_split(test_size=0.4)
+dataset = dataset_raw["train"].train_test_split(test_size=0.4)
 
 # Split full dataset into train, validation and test
-validation_set, test_set = itemgetter("train", "test")(mock_dataset["test"].train_test_split(test_size=0.5))
+validation_set, test_set = itemgetter("train", "test")(dataset["test"].train_test_split(test_size=0.5))
 
-mock_dataset["test"] = test_set
-mock_dataset["validation"] = validation_set
+dataset["test"] = test_set
+dataset["validation"] = validation_set
 
-# mock_dataset is the complete dataset
-print(mock_dataset)
+dataset.save_to_disk("./.generated_datasets")
